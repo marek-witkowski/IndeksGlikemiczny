9cf61ed98c2ea95f7e3d3e4b4ece93c24c23d0cb
Marek Witkowski <marek.witkowski.priv@gmail.com>
2023-11-10 14:55:44 +0100

    Seeds for Unit and ProductType - fixed problem with ID's.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,21 @@
 # frozen_string_literal: true
 
+
+
+
 Unit.destroy_all
+
 
 Unit.create!([
                { name: 'filiżanka' },
                { name: 'garść' },
                { name: 'kromka' },
                { name: 'liść' },
+               { name: 'łyżeczka' },
+               { name: 'łyżka' },
                { name: 'plaster' },
                { name: 'porcja' },
-               { name: 'szklanka' },
-               { name: 'łyżeczka' },
-               { name: 'łyżka' }
+               { name: 'szklanka' }
              ])
 
 Rails.logger.debug "DB: Create #{Unit.count} units."
